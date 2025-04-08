03a1cc6d18cd1fd39dab81391c8cc5df7f647eb4
Tom <8527295+OfficialTomJ@users.noreply.github.com>
2025-04-08 16:51:42 +1000

    Basic views and logic for GameView and StartView

--- BubblePop/Views/GameView.swift
+++ BubblePop/Views/GameView.swift
@@ -5,3 +5,34 @@
 //  Created by Tom on 8/4/2025.
 //
 
+import SwiftUI
+
+struct GameView: View {
+    @EnvironmentObject var game: GameState
+
+    var body: some View {
+        ZStack {
+            VStack {
+                Text("Player: \(game.playerName)")
+                Text("Score: \(game.score)")
+                Text("Time Left: \(game.timeRemaining)")
+                    .font(.headline)
+                    .padding(.bottom, 10)
+            }
+            .position(x: 100, y: 40)
+
+            ForEach(game.bubbles) { bubble in
+                Circle()
+                    .fill(bubble.color.color)
+                    .frame(width: 60, height: 60)
+                    .position(bubble.position)
+                    .onTapGesture {
+                        game.popBubble(bubble)
+                    }
+            }
+        }
+        .onAppear {
+            game.startGame()
+        }
+    }
+}
--- BubblePop/Views/StartView.swift
+++ BubblePop/Views/StartView.swift
@@ -5,3 +5,25 @@
 //  Created by Tom on 8/4/2025.
 //
 
+import SwiftUI
+
+struct StartView: View {
+    @EnvironmentObject var game: GameState
+    @State private var name = ""
+
+    var body: some View {
+        VStack(spacing: 20) {
+            Text("Enter your name:")
+            TextField("Name", text: $name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            Button("Start Game") {
+                game.playerName = name
+                game.startGame()
+            }
+            .disabled(name.isEmpty)
+        }
+        .padding()
+    }
+}
